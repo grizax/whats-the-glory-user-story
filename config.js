738ceef9ca1d1019a8cf6849dada0d32330c1121
grizax <ndbfoster@gmail.com>
2016-01-19 13:32:24 -0500

    Initial Commit

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+
+	"database": "mongodb://root:abc1234@ds047335.mongolab.com:47335/userstory_db_1",
+	"port": process.env.PORT || 3000,
+	"secretKey" : "Indigo 2020"
+
+}
---
+++ server.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+var config = require('./config');
+var mongoose = require('mongoose');
+
+mongoose.connect(config.database, function(err) {
+	if(err) {
+		console.log(err);
+	} else {
+		console.log('Connected to the database');
+	}
+});
+
+var app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(morgan('dev'));;
+
+
+
+var api = require('./app/routes/api')(app, express);
+app.use('/api', api);
+
+
+
+
+app.get('*', function(req, res) {
+	res.sendFile(__dirname + '/public/views/index.html');
+
+});
+
+
+app.listen(config.port, function(err) {
+	if(err) {
+		console.log(err);
+	} else {
+		console.log("Listening on port 3000");
+	}
+});
