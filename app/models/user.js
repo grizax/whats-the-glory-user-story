822e96d66caebc5752a307d00579fb638dd783ff
Nick Foster <grizax@users.noreply.github.com>
2016-03-01 18:46:28 -0500

    Creating API

--- app/models/user.js
+++ app/models/user.js
@@ -1,37 +1,41 @@
-var mongoose = require('mongoose');
+// Schemas
 
+var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var bcrypt = require('bcrypt-nodejs');
 
 var UserSchema = new Schema({
 	
-	name: String,
-	username: { type: String, required: false, index: { unique: true}},
-	password: { type: String, required: false, select: false}
+	name: String, // name doesn't have condition
+	username: { type: String, required: false, index: { unique: true}}, // username has conditions
+	password: { type: String, required: false, select: false} 	        // select = false because we don't want to query passwords
 
 });
 
+// Hash Password Field before we save to DB
 
-UserSchema.pre('save', function(next){
+UserSchema.pre('save', function(next){			    // 'pre' is Mongoose's middleware method to hash
 
-	var user = this;
-
-	if(!user.isModified('password')) return next();
-
-	bcrypt.hash(user.password, null, null, function(err, hash) {
-		if(err) return next(err);
-
-		user.password = hash;
+	var user = this;   								// refers to UserSchema object
+ 
+	if(!user.isModified('password')) return next(); // validation
+	// pass in user.password, null, null and a callback function with error and has
+	// error handling
+	bcrypt.hash(user.password, null, null, function(err, hash) { 
+		if(err) return next(err); 
+		user.password = hash;  // if there's no error, then hash user password
 		next();
 
 	});
 });
 
+// create custom method for UserSchema
 UserSchema.methods.comparePassword = function(password) {
 
-	var user = this;
+	var user = this; // refers to UserSchema object
 
 	return bcrypt.compareSync(password, user.password);
 
 }
+
 module.exports = mongoose.model('User', UserSchema);
--- app/routes/api.js
+++ app/routes/api.js
@@ -1,25 +1,47 @@
+// Call all important packages
+
 var User = require('../models/user');
-
 var config = require('../../config');
-
 var secretKey = config.secretKey;
+var jsonwebtoken = require('jsonwebtoken');
+
+function createToken(user) {
+
+	var token = jsonwebtoken.sign({
+		_id: user._id,
+		name: user.name,
+		username: user.username
+	}, secretKey, {
+		expiresInMinute: 1440
+	});
+	
+	return token;
+
+}
 
 module.exports = function(app, express) {
-
-
+	// call express function
 	var api = express.Router();
 
+	// first API is signup
 	api.post('/signup', function(req, res){
 
+			// create a new user
 			var user = new User({
 
+				// pull in data from user.js
 				name: req.body.name,
+				
+				// body parser
 				username: req.body.username,
 				password: req.body.password
 			});
 
+			// add callback function 
 			user.save(function(err){
 				if(err){
+
+					// error handling
 					res.send(err);
 					return;
 
@@ -28,8 +50,86 @@ module.exports = function(app, express) {
 			});
 	});
 
+	// Check if user already exists and handle that
+	api.get('/users', function(req, res) {
 
-	return api
+		// Mongoose find method which finds every user
+		User.find({}, function(err, users) {
+			if(err) {
+				res.send(err);
+				return;
+			}
+
+			res.json(users);
+
+		});
+	});
+	
+	// Create a login
+	api.post('/login', function(req, res) {
+
+		User.findOne({
+			username: req.body.username
+
+		}).select('password').exec(function(err, user) {
+			if(err) throw err;
+
+			if(!user) {
+				res.send({ message: "User doesn't exist!"});
+			} else if(user){
+				var validPassword = user.comparePassword(req.body.password);
+
+				if(!validPassword) {
+					res.send({ message: "Invalid password"});
+				} else {
+
+					// Token 
+					var token = createToken(user);
+
+					res.json({
+						sucess: true,
+						message: "Successfuly login!",
+						token: token
+					});
+				}
+			}
+		});
+	});
+
+	api.use(function(req, res, next) {
+
+		console.log("We have a visitor");
+
+		var token = req.body.token || req.param('token') || req.headers['x-access-token'];
+		
+		// Check if token exists
+		if(token) {
+
+			jsonwebtoken.verify(token, secretKey, function(err, decoded) {
+			
+				if(err) {
+					res.status(403).send({ success: false, message:"Failed to authenticate"});
+			
+				} else {
+					req.decoded = decoded;
+					next();
+				}
+			});
+		} else {
+
+			res.status(403).send({ success: false, message: "No Token Provided!"});
+		}
+	});
+
+	// Provide a Legitimate Token
+
+	// Create a Home API
+
+	api.get('/', function(req, res){
+		res.json("Illicit Successful Response!");
+	});
+
+	return api  
 
 
 }
